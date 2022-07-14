8fc8f6cc3fc2a4aec3fd90cee3cf380015eec29a
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-07-13 19:14:51 -0600

    copied about page from html file of old portfolio

---
+++ src/components/About.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const About = () => {
+  return (
+    <div id="about">
+      <h3>About</h3>
+      <div>
+        <p class="main-content">
+          I am an aspiring full stack developer. This is my first attempt to
+          build a portfolio page with which to showcase some of my work.
+          <br />
+          <br />I have over seven years of experience working in various roles
+          at a few manufacturing companies. My most recent job is my current
+          role as manufacturing engineer at a construction chemical company
+          specializing in polyurethane products called Master Builders
+          Solutions. Prior to that, I spent a little over two years working at
+          the Coors brewery in Golden, CO and four years at a Phillips 66 oil
+          refinery in Linden, NJ.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
