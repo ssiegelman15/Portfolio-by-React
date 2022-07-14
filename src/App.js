f1c11c9848f852c454a783573f77680794835616
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-07-13 18:57:18 -0600

    built App.js framework to passdown currentpage and changepage props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import './App.css';
-import React, {useState} from 'react';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import Projects from './components/Projects';
+import "./App.css";
+import React, { useState } from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("About");
+
+  // Render page based on state with About page as the default
+  const renderPage = () => {
+    if ((currentPage = Contact)) {
+      return <Contact />;
+    } else if ((currentPage = Projects)) {
+      return <Projects />;
+    } else {
+      return <About />;
+    }
+  };
+
+  const changePage = (page) => setCurrentPage(page);
+
   return (
-    
+    <div className="portfolio">
+      <div className="navbar">
+        <Navbar currentPage={currentPage} changePage={changePage} />
+      </div>
+      {renderPage()}
+      <Footer />
+    </div>
   );
 }
 
