fceb4324e333dd703da2d2bfdc1ee7d89aafaa89
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-07-15 11:18:56 -0600

    added text

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from 'react';
 
 const Contact = () => {
   return (
     <section id="contact">
-      <p>Contact page will be here eventually!</p>
+      <p>Contact page will be here eventually! Use Activity 16 in Week 20 for Reference.</p>
     </section>
   );
 };
