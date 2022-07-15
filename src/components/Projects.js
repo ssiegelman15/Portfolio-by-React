2666c68ccd30e88a31ed052730f15d17804f9156
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-07-14 18:29:25 -0600

    styled card a bit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/style-prop-object */
 import React from "react";
-import ParkPassImage from "../assets/images/Park-Pass-Screenshot.png";
+import ParkPassImage from "../assets/images/ParkPassScreenshot.png";
 
 const Projects = () => {
   return (
@@ -8,36 +8,36 @@ const Projects = () => {
       <h3>Work</h3>
       <aside>
         <div className="main-content d-flex row">
-          {/* <div className="card mx-2" style="width: 18rem;"> */}
-          <img
-            className="card-img-top"
-            src={ParkPassImage}
-            alt="Park Pass Landing Page"
-          />
-          <div className="card-body">
-            <h5 className="text-center">
+          <div className="card mx-2">
+            <img
+              className="card-img-top"
+              src={ParkPassImage}
+              alt="Park Pass Landing Page"
+            />
+            <div className="card-body">
+              <h5 className="text-center">
+                <a
+                  className="card-title text-white"
+                  href="https://gokublue007.github.io/Park-Pass/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  Park Pass
+                </a>
+              </h5>
+              <p className="card-text text-center">
+                Check out some trails near your favorite National Parks!
+              </p>
               <a
-                className="card-title text-white"
-                href="https://gokublue007.github.io/Park-Pass/"
+                href="https://github.com/gokublue007/Park-Pass"
+                className="btn btn-primary"
                 target="_blank"
                 rel="noreferrer noopener"
               >
-                Park Pass
+                Github Page
               </a>
-            </h5>
-            <p className="card-text text-center">
-              Check out some trails near your favorite National Parks!
-            </p>
-            <a
-              href="https://github.com/gokublue007/Park-Pass"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              Github Page
-            </a>
+            </div>
           </div>
-          {/* </div> */}
         </div>
       </aside>
     </section>
