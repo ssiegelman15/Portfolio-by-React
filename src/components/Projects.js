be6621d15916c07aaaaae1a0c468af81fb52a80c
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-07-15 09:18:26 -0600

    added more projects and styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,13 @@
 /* eslint-disable react/style-prop-object */
 import React from "react";
 import ParkPassImage from "../assets/images/ParkPassScreenshot.png";
+import BeerBlogImage from "../assets/images/BeerBlogScreenshot.png";
+import EarBudImage from "../assets/images/EarBudScreenshot.png";
 
 const Projects = () => {
   return (
     <section id="work">
-      <h3>Work</h3>
+      {/* <h3>Work</h3> */}
       <aside>
         <div className="main-content d-flex row">
           <div className="card mx-2">
@@ -38,6 +40,32 @@ const Projects = () => {
               </a>
             </div>
           </div>
+          <div class="card mx-2">
+              <img class="card-img-top" src={BeerBlogImage} alt="Beer Blog Homepage" />
+              <div class="card-body">
+                <h5 class="text-center">
+                  <a class="card-title text-white" href="https://radiant-meadow-34547.herokuapp.com/" target='_blank' rel="noreferrer noopener">
+                    Beer Blog
+                  </a>
+                </h5>
+                <p class="card-text text-center">Leave beers reviews and see what others have say as well!</p>
+                <a href="https://github.com/ssiegelman15/Beer-Blog" class="btn btn-primary" target='_blank' rel="noreferrer noopener">
+                  Github Page
+                </a>
+              </div>
+            </div>
+            <div class="card mx-2">
+              <img class="card-img-top" src={EarBudImage} alt="Earbud Login Page" />
+              <div class="card-body">
+                <h5 class="text-center">
+                  <a class="card-title text-white" href="https://safe-sea-62699.herokuapp.com/" target='_blank' rel="noreferrer noopener">
+                    EarBud
+                  </a>
+                </h5>
+                <p class="card-text text-center">Check out some trails near your favorite National Parks!</p>
+                <a href="https://github.com/NatalyaCo/EarBud" class="btn btn-primary" target='_blank' rel="noreferrer noopener">Github Page</a>
+              </div>
+            </div>
         </div>
       </aside>
     </section>
