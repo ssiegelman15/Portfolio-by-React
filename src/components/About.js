d5a68eb475e391b7c7f1655a5f4157ab211bb45a
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-07-15 10:38:29 -0600

    added 6 cards, need to change 4-6 to new projects

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,22 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import PictureOfMe from "../assets/images/PictureOfMe.jpg";
 
 const About = () => {
   return (
     <section id="about">
       <div>
+      <img
+        id="profile-pic"
+        src={PictureOfMe}
+        alt="Profile"
+        />
         <p className="main-content">
           I am an aspiring full stack developer. This is my first attempt to
-          build a portfolio page with which to showcase some of my work.
-          <br />
-          <br />I have over seven years of experience working in various roles
+          build a portfolio page using React.js with which to showcase some of my work.
+          I have over seven years of experience working in various roles
           at a few manufacturing companies. My most recent job is my current
-          role as manufacturing engineer at a construction chemical company
+          role as production engineer at a construction chemical company
           specializing in polyurethane products called Master Builders
           Solutions. Prior to that, I spent a little over two years working at
           the Coors brewery in Golden, CO and four years at a Phillips 66 oil
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -65,6 +65,62 @@ const Projects = () => {
                 <a href="https://github.com/NatalyaCo/EarBud" class="btn btn-primary" target='_blank' rel="noreferrer noopener">Github Page</a>
               </div>
             </div>
+          <div className="card mx-2">
+            <img
+              className="card-img-top"
+              src={ParkPassImage}
+              alt="Park Pass Landing Page"
+            />
+            <div className="card-body">
+              <h5 className="text-center">
+                <a
+                  className="card-title text-white"
+                  href="https://gokublue007.github.io/Park-Pass/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  Park Pass
+                </a>
+              </h5>
+              <p className="card-text text-center">
+                Check out some trails near your favorite National Parks!
+              </p>
+              <a
+                href="https://github.com/gokublue007/Park-Pass"
+                className="btn btn-primary"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Github Page
+              </a>
+            </div>
+          </div>
+          <div class="card mx-2">
+              <img class="card-img-top" src={BeerBlogImage} alt="Beer Blog Homepage" />
+              <div class="card-body">
+                <h5 class="text-center">
+                  <a class="card-title text-white" href="https://radiant-meadow-34547.herokuapp.com/" target='_blank' rel="noreferrer noopener">
+                    Beer Blog
+                  </a>
+                </h5>
+                <p class="card-text text-center">Leave beers reviews and see what others have say as well!</p>
+                <a href="https://github.com/ssiegelman15/Beer-Blog" class="btn btn-primary" target='_blank' rel="noreferrer noopener">
+                  Github Page
+                </a>
+              </div>
+            </div>
+            <div class="card mx-2">
+              <img class="card-img-top" src={EarBudImage} alt="Earbud Login Page" />
+              <div class="card-body">
+                <h5 class="text-center">
+                  <a class="card-title text-white" href="https://safe-sea-62699.herokuapp.com/" target='_blank' rel="noreferrer noopener">
+                    EarBud
+                  </a>
+                </h5>
+                <p class="card-text text-center">Check out some trails near your favorite National Parks!</p>
+                <a href="https://github.com/NatalyaCo/EarBud" class="btn btn-primary" target='_blank' rel="noreferrer noopener">Github Page</a>
+              </div>
+            </div>
         </div>
       </aside>
     </section>
