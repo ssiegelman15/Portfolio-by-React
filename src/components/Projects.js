80ffd2edd021c5e1896783e3fea3eca616d1a110
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-07-16 13:06:06 -0600

    styled projects page to show 3 projects per line in full screen size

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,10 +6,10 @@ import EarBudImage from "../assets/images/EarBudScreenshot.png";
 
 const Projects = () => {
   return (
-    <section id="work">
-      <aside>
-        <div className="main-content d-flex row ">
-          <div className="card mx-2 ">
+    <section>
+      <div className="container">
+        <div className="main-content row">
+          <div className="card mx-2 col">
             <img
               className="card-img-top"
               src={ParkPassImage}
@@ -39,54 +39,28 @@ const Projects = () => {
               </a>
             </div>
           </div>
-          <div className="card mx-2">
-              <img className="card-img-top" src={BeerBlogImage} alt="Beer Blog Homepage" />
-              <div className="card-body">
-                <h5 className="text-center">
-                  <a className="card-title text-white" href="https://radiant-meadow-34547.herokuapp.com/" target='_blank' rel="noreferrer noopener">
-                    Beer Blog
-                  </a>
-                </h5>
-                <p className="card-text text-center">Leave beers reviews and see what others have say as well!</p>
-                <a href="https://github.com/ssiegelman15/Beer-Blog" className="btn btn-primary" target='_blank' rel="noreferrer noopener">
-                  Github Page
-                </a>
-              </div>
-            </div>
-            <div className="card mx-2">
-              <img className="card-img-top" src={EarBudImage} alt="Earbud Login Page" />
-              <div className="card-body">
-                <h5 className="text-center">
-                  <a className="card-title text-white" href="https://safe-sea-62699.herokuapp.com/" target='_blank' rel="noreferrer noopener">
-                    EarBud
-                  </a>
-                </h5>
-                <p className="card-text text-center">Check out some trails near your favorite National Parks!</p>
-                <a href="https://github.com/NatalyaCo/EarBud" className="btn btn-primary" target='_blank' rel="noreferrer noopener">Github Page</a>
-              </div>
-            </div>
-          <div className="card mx-2">
+          <div className="card mx-2 col">
             <img
               className="card-img-top"
-              src={ParkPassImage}
-              alt="Park Pass Landing Page"
+              src={BeerBlogImage}
+              alt="Beer Blog Homepage"
             />
             <div className="card-body">
               <h5 className="text-center">
                 <a
                   className="card-title text-white"
-                  href="https://gokublue007.github.io/Park-Pass/"
+                  href="https://radiant-meadow-34547.herokuapp.com/"
                   target="_blank"
                   rel="noreferrer noopener"
                 >
-                  Park Pass
+                  Beer Blog
                 </a>
               </h5>
               <p className="card-text text-center">
-                Check out some trails near your favorite National Parks!
+                Leave beers reviews and see what others have say as well!
               </p>
               <a
-                href="https://github.com/gokublue007/Park-Pass"
+                href="https://github.com/ssiegelman15/Beer-Blog"
                 className="btn btn-primary"
                 target="_blank"
                 rel="noreferrer noopener"
@@ -95,34 +69,130 @@ const Projects = () => {
               </a>
             </div>
           </div>
-          <div className="card mx-2">
-              <img className="card-img-top" src={BeerBlogImage} alt="Beer Blog Homepage" />
-              <div className="card-body">
-                <h5 className="text-center">
-                  <a className="card-title text-white" href="https://radiant-meadow-34547.herokuapp.com/" target='_blank' rel="noreferrer noopener">
-                    Beer Blog
-                  </a>
-                </h5>
-                <p className="card-text text-center">Leave beers reviews and see what others have say as well!</p>
-                <a href="https://github.com/ssiegelman15/Beer-Blog" className="btn btn-primary" target='_blank' rel="noreferrer noopener">
-                  Github Page
+          <div className="card mx-2 col">
+            <img
+              className="card-img-top"
+              src={EarBudImage}
+              alt="Earbud Login Page"
+            />
+            <div className="card-body">
+              <h5 className="text-center">
+                <a
+                  className="card-title text-white"
+                  href="https://safe-sea-62699.herokuapp.com/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  EarBud
                 </a>
-              </div>
+              </h5>
+              <p className="card-text text-center">
+                Check out some trails near your favorite National Parks!
+              </p>
+              <a
+                href="https://github.com/NatalyaCo/EarBud"
+                className="btn btn-primary"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Github Page
+              </a>
             </div>
-            <div className="card mx-2">
-              <img className="card-img-top" src={EarBudImage} alt="Earbud Login Page" />
-              <div className="card-body">
-                <h5 className="text-center">
-                  <a className="card-title text-white" href="https://safe-sea-62699.herokuapp.com/" target='_blank' rel="noreferrer noopener">
-                    EarBud
-                  </a>
-                </h5>
-                <p className="card-text text-center">Check out some trails near your favorite National Parks!</p>
-                <a href="https://github.com/NatalyaCo/EarBud" className="btn btn-primary" target='_blank' rel="noreferrer noopener">Github Page</a>
-              </div>
-            </div>
+          </div>
         </div>
-      </aside>
+        <div className="main-content row">
+          <div className="card mx-2 col">
+            <img
+              className="card-img-top"
+              src={ParkPassImage}
+              alt="Park Pass Landing Page"
+            />
+            <div className="card-body">
+              <h5 className="text-center">
+                <a
+                  className="card-title text-white"
+                  href="https://gokublue007.github.io/Park-Pass/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  Park Pass
+                </a>
+              </h5>
+              <p className="card-text text-center">
+                Check out some trails near your favorite National Parks!
+              </p>
+              <a
+                href="https://github.com/gokublue007/Park-Pass"
+                className="btn btn-primary"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Github Page
+              </a>
+            </div>
+          </div>
+          <div className="card mx-2 col">
+            <img
+              className="card-img-top"
+              src={BeerBlogImage}
+              alt="Beer Blog Homepage"
+            />
+            <div className="card-body">
+              <h5 className="text-center">
+                <a
+                  className="card-title text-white"
+                  href="https://radiant-meadow-34547.herokuapp.com/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  Beer Blog
+                </a>
+              </h5>
+              <p className="card-text text-center">
+                Leave beers reviews and see what others have say as well!
+              </p>
+              <a
+                href="https://github.com/ssiegelman15/Beer-Blog"
+                className="btn btn-primary"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Github Page
+              </a>
+            </div>
+          </div>
+          <div className="card mx-2 col">
+            <img
+              className="card-img-top"
+              src={EarBudImage}
+              alt="Earbud Login Page"
+            />
+            <div className="card-body">
+              <h5 className="text-center">
+                <a
+                  className="card-title text-white"
+                  href="https://safe-sea-62699.herokuapp.com/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  EarBud
+                </a>
+              </h5>
+              <p className="card-text text-center">
+                Check out some trails near your favorite National Parks!
+              </p>
+              <a
+                href="https://github.com/NatalyaCo/EarBud"
+                className="btn btn-primary"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Github Page
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
